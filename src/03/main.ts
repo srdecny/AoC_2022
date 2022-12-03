a22c9c1b2a85a80d68a7466accb2906234f3f7c4
Vojtěch Srdečný <vojtech.srdecny@gmail.com>
2022-12-03 19:01:17 +0100

    Cursed day3 alt solution

--- src/03/main.ts
+++ src/03/main.ts
@@ -40,6 +40,37 @@ function part2(input: string): number {
   return commonInGroups.reduce((acc, char) => acc + score(char), 0);
 }
 
+// match 0 char, group with 1 char, match length - 1 chars
+// match 1 char, group with 1 char, match length - 2 chars
+// ...
+// the group is the item we're checking
+// it is either (.) if in the irst half or \1 in the second half
+function* allPositions(length: number) {
+	for (let i = 0; i < length; i++) {
+		const prefix = `.{${i}}`
+		const postfix = `.{${length - i - 1}}`
+		yield (group: string) => prefix + group + postfix
+	}
+}
+
+function part1_alt(input: string): number {
+	const lengths = [...new Set(input.split("\n").map(line => line.length))]
+	let groupCounter = 1;
+	const regexesForLengths = lengths.map(fullLength => {
+		const halfLength = fullLength / 2
+		const regexes = []
+		for (const firstHalf of allPositions(halfLength)) {
+			for (const secondHalf of allPositions(halfLength)) {
+				regexes.push(firstHalf("(.)") + secondHalf(`\\${groupCounter++}`))
+			}
+		}
+		return regexes.map(r => `^${r}$`).join("|")
+	})
+	const regex = new RegExp(regexesForLengths.join("|"))
+	const matches = input.split("\n").map(line => line.match(regex).splice(1).find(x => x))
+	return matches.reduce((acc, match) => acc + score(match[0]), 0)
+}
+
 // alternate part2 solution
 function part2_alt(input: string): number {
   return input.replaceAll(/.*(.).*\n.*\1.*\n.*\1.*/g, "$1").split("\n").reduce((acc, char) => acc + score(char), 0);
@@ -48,5 +79,6 @@ function part2_alt(input: string): number {
 runTests("tests.json", part1, part2);
 const input = readFileSync("input.txt", "utf8").trim();
 console.log("Part 1: " + part1(input));
+console.log("Part 1 (alt): " + part1_alt(input));
 console.log("Part 2: " + part2(input));
 console.log("Part 2 (alt): " + part2_alt(input));
